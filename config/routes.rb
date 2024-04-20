c051abbd0f6b5df4c3b4688f98a9680a29dc3822
Mitsuki Yanagi <iganaymjs2@gmail.com>
2024-04-20 20:07:41 +0900

    fix:blogs/:id（blog_path(b)）というURLをBlogsコントローラのshowアクションを呼び出すようにルート設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'blogs', to: 'blogs#index'
+  get 'blogs/:id', to: 'blogs#show', as: 'blog'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
