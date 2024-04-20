9ba162195fe61567d2ef96b4d690a3a96de90667
Mitsuki Yanagi <iganaymjs2@gmail.com>
2024-04-20 19:11:32 +0900

    fix:blogsテーブルに値を追加できていなかったので、再々修正した

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Blog.create(name: 工藤新一, title: 俺の決め台詞, body: 真実はいつもひとつ！)
-Blog.create(name: 江戸川コナン, title: 僕の決め台詞, body: 江戸川コナン、探偵さ！)
-Blog.create(name: 毛利蘭, title: 私の決め台詞, body: しんいち〜！！)
-Blog.create(name: 毛利小五郎, title: 俺の決め台詞, body: その眠りの小五郎とは、私のことです！)
-Blog.create(name: 灰原哀, title: 私の決め台詞, body: バカね、江戸川君。)
+Blog.create(name: "工藤新一", title: "俺の決め台詞", body: "真実はいつもひとつ！")
+Blog.create(name: "江戸川コナン", title: "僕の決め台詞", body: "江戸川コナン、探偵さ！")
+Blog.create(name: "毛利蘭", title: "私の決め台詞", body: "しんいち〜！！")
+Blog.create(name: "毛利小五郎", title: "俺の決め台詞", body: "その眠りの小五郎とは、私のことです！")
+Blog.create(name: "灰原哀", title: "私の決め台詞", body: "バカね、江戸川君。")
