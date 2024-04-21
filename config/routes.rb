598eeaae09996cf2f610f2e34c0d11554bf7ba1c
Mitsuki Yanagi <iganaymjs2@gmail.com>
2024-04-21 10:15:30 +0900

    feat:edit.html.erbのhtmlを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'blogs', to: 'blogs#index'
   get 'blogs/:id', to: 'blogs#show', as: 'blog'
+  get 'blogs/:id/edit', to: 'blogs#edit', as: 'edit_blog'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
