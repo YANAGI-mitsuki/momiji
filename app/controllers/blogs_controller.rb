2749b4dc7ef045fcfb6cec74534f0680e92e3ba7
Mitsuki Yanagi <iganaymjs2@gmail.com>
2024-04-21 09:19:55 +0900

    feat:Blogsコントローラにeditアクションとupdateアクションを追加した

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,4 +5,20 @@ class BlogsController < ApplicationController
     def show
         @blog = Blog.find(params[:id])
     end
+    def edit
+        @blog = Blog.find(params[:id])
+    end
+    def update
+        @blog = Blog.find(params[:id])
+        if @blog = Blog.update(blog_params)
+            redirect_to blog_path
+        else
+            render 'edit'
+        end
+    end
+
+    private
+    def blog_params
+        params.require(:blog).permit(:name, :title, :body)
+    end
 end
